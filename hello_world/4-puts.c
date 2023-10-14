8f18f17c781847aa3042b0886582df76d4c61ea3
Rufat2314 <rufat1987@gmail.com>
2023-10-14 13:39:52 -0700

    4. Hello, puts4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * Write text through calling a puts function 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
