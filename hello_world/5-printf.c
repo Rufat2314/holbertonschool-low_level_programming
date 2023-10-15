efc3d82c97764692636930b5fa7b7dee1dab6e34
Rufat2314 <rufat1987@gmail.com>
2023-10-15 01:42:03 -0700

    5. Hello, printf4

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,15 +1,19 @@
-/**
- * Includes library for preprocessing 
+/*
+ * File Name: 5-printf.c
+ * Holberton tasks
  */
 
 #include <stdio.h>
 
 /**
- * Print "with proper grammar, but the outcome is
- *        a piece of art,"
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
