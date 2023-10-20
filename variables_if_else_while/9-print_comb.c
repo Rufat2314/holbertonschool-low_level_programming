4c872e7fcf49a17521d848104dc809617b825527
Rufat2314 <rufat1987@gmail.com>
2023-10-20 07:56:56 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
