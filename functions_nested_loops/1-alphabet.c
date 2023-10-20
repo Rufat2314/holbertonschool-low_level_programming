d55954ca9bcdfcb51e95970cb2727c625a14cbb1
Rufat2314 <rufat1987@gmail.com>
2023-10-20 12:41:28 -0700

    1. I sometimes suffer from insomnia.

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
