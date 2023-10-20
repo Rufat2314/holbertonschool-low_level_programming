fd534e0d43def71ade44a79d7e2093ee23761b04
Rufat2314 <rufat1987@gmail.com>
2023-10-20 02:56:06 -0700

    5. Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+File Edit Options Buffers Tools C Help
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+        int i;
+
+        for (i = 0; i < 10; i++)
+        {
+	  printf("%d", i);
+        }
+
+        return (0);
+}
