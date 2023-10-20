e4d96423209c431d0648a49de75f5bde7574b589
Rufat2314 <rufat1987@gmail.com>
2023-10-20 06:59:35 -0700

    6. Numberz2

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,11 @@
 int main(void)
 {
 	int i;
-        for (i = '0'; i <= '9'; i++)
-        {
-                putchar(i);
-        }
-        putchar('\n');
-        return (0);
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
