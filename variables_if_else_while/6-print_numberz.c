53544bd604382ef97be7d843ba69f8982b5a5fab
Rufat2314 <rufat1987@gmail.com>
2023-10-20 06:44:02 -0700

    6. Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	for (int i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+        }
+	putchar("\n");
+	return (0);
+}
