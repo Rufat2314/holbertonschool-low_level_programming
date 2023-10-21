81d012c86b077cae079ea14e536122486540941f
Rufat2314 <rufat1987@gmail.com>
2023-10-21 03:09:05 -0700

    10. a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - checks for lowercase character.
+ * @n: The character to be checked.
+ * @m: adds two integers and returns the result.
+ *
+ * Return: Always(Success)
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
