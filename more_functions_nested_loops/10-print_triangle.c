24679660edbf05cd9e3ad450829e2c4d808b2848
Rufat2314 <rufat1987@gmail.com>
2023-10-22 08:36:37 -0700

    10. Triangles2

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -13,11 +13,15 @@ void print_triangle(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (a = 0; a < row; a++)
+			{
 				_putchar(' ');
 				row--;
+			}
 
 			for (j = 0; j < (i + 1); j++)
+			{
 				_putchar('#');
+			}
 
 			_putchar('\n');
 		}
