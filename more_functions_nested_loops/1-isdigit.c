55cbd62f20b711c889303d216363fe13b566765e
Rufat2314 <rufat1987@gmail.com>
2023-10-22 01:29:08 -0700

    1. isdigit1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _isupper - prints the last digit of a number.
+ * _isdigit - prints the last digit of a number.
  * @c: The character to be checked.
  *
  * Return: Always(Success)
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
