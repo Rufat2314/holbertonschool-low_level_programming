c5f4aaa3bdc03a7c51beeb907fba4414f21cbe0b
Rufat2314 <rufat1987@gmail.com>
2023-10-22 01:34:02 -0700

    2. Collaboration is multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - prints the last digit of a number.
+ * @a: The character to be checked.
+ * @b: The character to be checked.
+ *
+ * Return: Always(Success)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
