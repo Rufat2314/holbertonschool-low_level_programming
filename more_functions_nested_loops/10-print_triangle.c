d55ce007eb8abe640f93c050823955fb3e236166
Rufat2314 <rufat1987@gmail.com>
2023-10-22 08:41:29 -0700

    10. Triangles3

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,32 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * print_triangle - Draws a straight line using the character _.
- * @size: The number of _ characters to be printed.
+ * print_triangle - Prints a triangle, using the character #.
+ * @size: The size of the triangle.
  */
 void print_triangle(int size)
 {
-	int a, i, j;
-	int row = (size - 1);
+	int hash, index;
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (hash = 1; hash <= size; hash++)
 		{
-			for (a = 0; a < row; a++)
-			{
+			for (index = size - hash; index > 0; index--)
 				_putchar(' ');
-				row--;
-			}
 
-			for (j = 0; j < (i + 1); j++)
-			{
+			for (index = 0; index < hash; index++)
 				_putchar('#');
-			}
+
+			if (hash == size)
+				continue;
 
 			_putchar('\n');
 		}
 	}
-	else
-	{
-		_putchar('\n');
-	}
+
+	_putchar('\n');
 }
+
+
+
